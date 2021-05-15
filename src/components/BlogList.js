a531f37378973f3953fe629e77c2a634578d9907
chandima <chandimaofficial@gmail.com>
2021-05-15 20:12:47 +0530

    add delete book button

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,15 +1,16 @@
 const BlogList = (props) => {
   const blogs = props.blogs;
   const title = props.title;
-  console.log(props, blogs);
+  const handleDelete = props.handleDelete;
 
   return (
     <div className="blog-list">
-        <h2>{title}</h2>
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by : {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,20 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
-  const [blogs,setBlogs] = useState([
-    {title:'new web' ,body:'asdasd' , author:'Mahela' , id: '1'},
-    {title:'welcome' ,body:'asdasd' , author:'chandima' , id: '2'},
-    {title:'party ass' ,body:'asdasd' , author:'Mahela' , id: '3'},
+  const [blogs, setBlogs] = useState([
+    { title: "new web", body: "asdasd", author: "Mahela", id: "1" },
+    { title: "welcome", body: "asdasd", author: "chandima", id: "2" },
+    { title: "party ass", body: "asdasd", author: "Mahela", id: "3" },
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
 
   return (
     <div className="home">
-      <BlogList blogs= {blogs}  title = {"All Blogs"}/>
-      <BlogList blogs= {blogs.filter((blog) => blog.author==='Mahela')}  title = {"Mahela's Blogs"}/>
+      <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
     </div>
   );
 };
