f3f0545a37793800c863b95f8f48d3ebbfe3f7b5
chandima <chandimaofficial@gmail.com>
2021-05-15 19:01:26 +0530

    used filter in arrays to filter blogs

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,15 @@ import BlogList from "./BlogList";
 const Home = () => {
 
   const [blogs,setBlogs] = useState([
-    {title:'new web' ,body:'asdasd' , author:'mahela' , id: '1'},
+    {title:'new web' ,body:'asdasd' , author:'Mahela' , id: '1'},
     {title:'welcome' ,body:'asdasd' , author:'chandima' , id: '2'},
-    {title:'party ass' ,body:'asdasd' , author:'siri' , id: '3'},
+    {title:'party ass' ,body:'asdasd' , author:'Mahela' , id: '3'},
   ]);
 
   return (
     <div className="home">
       <BlogList blogs= {blogs}  title = {"All Blogs"}/>
+      <BlogList blogs= {blogs.filter((blog) => blog.author==='Mahela')}  title = {"Mahela's Blogs"}/>
     </div>
   );
 };
