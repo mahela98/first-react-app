fc20cc3e6ce957181c71614b18651a417be51b54
chandima <chandimaofficial@gmail.com>
2021-05-16 08:59:13 +0530

    used useEffect hook - useEffrct function reaturns nothing and runs every time page renders

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,11 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  //runs every time page renders
+  useEffect(() => {
+    console.log("use effect ran");
+  });
 
   return (
     <div className="home">
