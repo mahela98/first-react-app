b08966ab7f1d6610126e833c806747e2ba84590e
chandima <chandimaofficial@gmail.com>
2021-05-16 10:59:06 +0530

    added a loading message if data fetching take time

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,24 +3,29 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending,setIsPending] = useState(true);
 
   //runs every time page renders
   useEffect(() => {
     console.log("use effect ran");
-    fetch("  http://localhost:8000/blogs")
+    setTimeout(()=> {
+      fetch("  http://localhost:8000/blogs")
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         console.log(data);
         setBlogs(data);
+        setIsPending(false);
       });
+    },1000);
   }, []);
   //runs only if the name changes
 
   return (
     <div className="home">
       {/* this only works if the blogs array is not empty */}
+      {isPending && <div> Loading... </div>}
       {blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
     </div>
   );
