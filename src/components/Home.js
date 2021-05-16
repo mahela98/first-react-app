8c5e4c61a06e3f646c981083c14023cdd7bc6749
chandima <chandimaofficial@gmail.com>
2021-05-16 11:37:34 +0530

    add error handlers

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,22 +3,32 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-  const [isPending,setIsPending] = useState(true);
-
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
   //runs every time page renders
   useEffect(() => {
     console.log("use effect ran");
-    setTimeout(()=> {
+    setTimeout(() => {
       fetch("  http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-        setIsPending(false);
-      });
-    },1000);
+        .then((res) => {
+          console.log(res);
+          if (!res.ok) {
+            throw Error("could not fetch the data");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          // console.log(err.message);
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
   }, []);
   //runs only if the name changes
 
@@ -26,6 +36,8 @@ const Home = () => {
     <div className="home">
       {/* this only works if the blogs array is not empty */}
       {isPending && <div> Loading... </div>}
+      { error && <div>{error}</div>}
+
       {blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
     </div>
   );
