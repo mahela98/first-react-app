cfc1d09e22454cecaaebee11dc986dd7ea4c22c9
chandima <chandimaofficial@gmail.com>
2021-05-16 10:45:07 +0530

    fetching data from db.json file and display

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,31 +2,26 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "new web", body: "asdasd", author: "Mahela", id: "1" },
-    { title: "welcome", body: "asdasd", author: "chandima", id: "2" },
-    { title: "party ass", body: "asdasd", author: "Mahela", id: "3" },
-  ]);
-
-    const [name,setName] = useState('mario');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   //runs every time page renders
   useEffect(() => {
     console.log("use effect ran");
-    console.log(name);
-  },[name]);
+    fetch("  http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
   //runs only if the name changes
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
-      <button onClick={()=>{setName('Mahela')}}>Change Name</button>
-      <p>{name}</p>
+      {/* this only works if the blogs array is not empty */}
+      {blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
     </div>
   );
 };
