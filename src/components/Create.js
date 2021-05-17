455877e9e866a4078913b78be4bb34c81f0fb82c
chandima <chandimaofficial@gmail.com>
2021-05-17 12:03:34 +0530

    created blog create form

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,24 @@
 const Create = () => {
-    return ( 
-        <h2>Add a new Blog</h2>
-     );
-}
- 
+  return (
+    <div className="create">
+      <h2>Add a new Blog</h2>
+      <form>
+          <label> Blog title: </label>
+          <input type="text" 
+          required
+          />
+          <label> Blog Body: </label>
+          <textarea required ></textarea>
+
+          <label> Blog Author: </label>
+          <select>
+              <option value="Mahela">Mahela</option>
+              <option value="Chandima">Chandima</option>
+          </select>
+          <button>Add Blog</button>
+      </form>
+    </div>
+  );
+};
+
 export default Create;
