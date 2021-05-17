00fc710ad8aec7d2736819c91ee743f847a4a21c
chandima <chandimaofficial@gmail.com>
2021-05-17 18:56:05 +0530

    created blog object

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,38 +1,44 @@
-import {useState} from 'react';
+import { useState } from "react";
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Mahela");
 
-    const [title,setTitle] = useState('');
-    const [body,setBody] = useState('');
-    const [author,setAuthor] = useState('Mahela');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
 
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
-      <form>
-          <label> Blog title: </label>
-          <input type="text" 
+      <form onSubmit={handleSubmit}>
+        <label> Blog title: </label>
+        <input
+          type="text"
           required
-          value = {title}
-          onChange = {(e) =>setTitle(e.target.value)}
-          />
-          <label> Blog Body: </label>
-          <textarea required 
-              value = {body}
-              onChange = {(e) =>setBody(e.target.value)}
-          ></textarea>
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label> Blog Body: </label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
 
-          <label> Blog Author: </label>
-          <select
-          value = {author}
-          onChange = {(e)=>{setAuthor(e.target.value)}}
-          >
-              <option value="Mahela">Mahela</option>
-              <option value="Chandima">Chandima</option>
-          </select>
-          <button>Add Blog</button>
-          <p>{title}</p>
-          <p>{body}</p>
-          <p>{author}</p>
+        <label> Blog Author: </label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        >
+          <option value="Mahela">Mahela</option>
+          <option value="Chandima">Chandima</option>
+        </select>
+        <button>Add Blog</button>
       </form>
     </div>
   );
